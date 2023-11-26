339087d7a98d58f424273d372515997c7e3dc049
Viktor Tochonov <viktochonov@gmail.com>
2023-11-26 18:18:52 +0200

    # Task 6.  DatabaseContext,Sessions,Repository usings have added to Program.cs file

--- Reviews_AJAX/Program.cs
+++ Reviews_AJAX/Program.cs
@@ -1,8 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
+using Reviews_AJAX.Models;
+using Reviews_AJAX.Repos;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+// Отримуємо рядок підключення з конфігураційного файлу
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// додаємо контекст ApplicationContext як сервіс у додаток
+builder.Services.AddDbContext<ReviewContext>(options => options.UseSqlServer(connection));
+builder.Services.AddDistributedMemoryCache();
+// додаємо залежність від репозиторію, для опосередкованого керування БД
+builder.Services.AddScoped<IRepository, ReviewsRepository>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSession(opt =>
+{
+    opt.IdleTimeout = TimeSpan.FromMinutes(30);
+    opt.Cookie.Name = "Session";
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -12,7 +27,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
