2dbd35b41dbcf783873d41bb12f80449c5e3f835
Колега-розробник <orions3171@ukr.net>
2023-11-26 17:48:57 +0200

    # Task 2. New models have been added - User and Review

---
+++ Reviews_AJAX/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace Reviews_AJAX.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string ReviewText { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Reviews_AJAX/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Reviews_AJAX.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public ICollection<Review>? Reviews { get; set; }
+    }
+}
