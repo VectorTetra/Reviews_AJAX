6d2405c00374617f110a720fafa3d1de95e14e0a
Колега-розробник <orions3171@ukr.net>
2023-11-26 17:59:43 +0200

    # Task 3. New database context has added - Review Context

---
+++ Reviews_AJAX/Models/ReviewContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+
+namespace Reviews_AJAX.Models
+{
+    public class ReviewContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public ReviewContext(DbContextOptions<ReviewContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
