e760d33943627f0c423d249992ce7bc2b8d069ce
Viktor Tochonov <viktochonov@gmail.com>
2023-11-27 21:29:18 +0200

    # Task 8. Trying find out, why cannot get reviews or login;

--- Reviews_AJAX/Controllers/HomeController.cs
+++ Reviews_AJAX/Controllers/HomeController.cs
@@ -1,21 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using Reviews_AJAX.Models;
+using Reviews_AJAX.Repos;
 using System.Diagnostics;
 
 namespace Reviews_AJAX.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IRepository repo;
+        public HomeController(IRepository repo)
         {
-            _logger = logger;
+            this.repo = repo;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginVM loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                var fullname = await repo.TryToLogin(loginVM);
+                if (fullname != null)
+                {
+                    HttpContext.Session.SetString("FullName", fullname);
+                    HttpContext.Session.SetString("login", loginVM.Login);
+                    string response = fullname;
+                    return Json(response);
+                }
+                else
+                {
+                    return Problem("Неправильний логін або пароль");
+                }
+            }
+            return Problem("Помилка під час відправки даних");
+        }
+        //public IActionResult Register()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public async Task<IActionResult> GetReviews()
+        {
+            var Reviews = await repo.GetReviews();
+            string response = JsonConvert.SerializeObject(Reviews);
+            return Json(response);
         }
     }
 }
